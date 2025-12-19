b7e3280b4a0b8b6e87b22a8d5d21453630b06de6
ngzuwayne <140599260+ngzuwayne@users.noreply.github.com>
2025-12-19 19:29:22 +0800

    Added a route guard

--- money-tracker-ui/src/app/app.routes.ts
+++ money-tracker-ui/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { HomeComponent } from './pages/home/home';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] }
 ];
---
+++ money-tracker-ui/src/app/guards/auth-guard.ts
@@ -0,0 +1,17 @@
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    return true;
+  }
+
+  // ❌ not logged in → redirect to login
+  router.navigate(['/']);
+  return false;
+};
